4f8a13038695bc4c3d548e8b9d42e7836fe7fd7f
Silvian-73 <yurykuz65@gmail.com>
2020-08-26 16:40:46 +0300

    Сделал перемещение игрока

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(UnitMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private const int LEFT_MOUSE_BUTTON = 0;
+
+    [SerializeField] private GameObject _spinningArrowPrefab;
+    [SerializeField] private float _spinningArrowLifetime;
+    [SerializeField] private LayerMask _groundMask;
+
+    private UnitMotor _motor;
+
+    private void Awake()
+    {
+        _motor = GetComponent<UnitMotor>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            bool raycastSucessful = Physics.Raycast(ray, out raycastHit, Mathf.Infinity, _groundMask);
+            if(raycastSucessful)
+            {
+                Vector3 target = raycastHit.point;
+                _motor.SetDestination(target);
+                GameObject spinningArrow = GameObject.Instantiate(_spinningArrowPrefab);
+                spinningArrow.transform.position += target;
+                Object.Destroy(spinningArrow, _spinningArrowLifetime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnitMotor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class UnitMotor : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetDestination(Vector3 target)
+    {
+        _navMeshAgent.SetDestination(target);
+    }
+}
