2e91be7a37d08b347d09b546c631bef8cdf6cf9c
Silvian-73 <yurykuz65@gmail.com>
2020-09-04 18:16:57 +0300

    Сделал анимацию Idle и ходьбы

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool("Moving", _navMeshAgent.velocity != Vector3.zero);
+    }
+}
